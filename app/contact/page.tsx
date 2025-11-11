598228eccfcfd1519b616b0bc6f0df1a3193fcf2
Greg Carroll <gregoryc8@bigpond.com>
2025-11-11 10:30:48 +1000

    change email to gregory@GMC

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
   return (
     <article className="prose prose-invert max-w-none">
       <h1 className="text-3xl font-bold text-orange-500 mb-4">Contact</h1>
-      <p>Email: <a className="underline" href="mailto:info@gregorymcarroll.com">info@gregorymcarroll.com</a></p>
+      <p>Email: <a className="underline" href="mailto:gregory@gregorymcarroll.com">gregory@gregorymcarroll.com</a></p>
     </article>
   );
 }
